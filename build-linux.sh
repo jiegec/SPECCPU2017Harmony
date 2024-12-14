454c9932a85697cf8fa9c8b0fc1213f8456fbea9
Jiajie Chen <c@jia.je>
2024-12-14 22:10:36 +0800

    Simplify linux setup

--- build-linux.sh
+++ build-linux.sh
@@ -8,8 +8,8 @@ export DEVECO_SDK_HOME=$TOOL_HOME/sdk
 export PATH=$TOOL_HOME/ohpm/bin:$PATH
 export PATH=$TOOL_HOME/hvigor/bin:$PATH
 export PATH=$TOOL_HOME/node/bin:$PATH
-# assume arkui-x directory created at $HOME/arkui-x
-# LICENSE & LICENSE.sha256 copied from macOS ~/Library/ArkUI-X/Sdk/licenses
-# to Linux $HOME/arkui-x/licenses
+# assume arkui-x directory to be saved at $HOME/arkui-x
 export ARKUIX_SDK_HOME=$HOME/arkui-x
+mkdir -p $ARKUIX_SDK_HOME/licenses
+cp -r .ci/* $ARKUIX_SDK_HOME/licenses
 hvigorw assembleHap
